2630335e34aa291eb75ceb01501245b94f3e14bc
Angelo <vino_1105@hotmail.com>
2021-02-09 18:16:15 -0800

    updated app js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Discord Explore University
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
