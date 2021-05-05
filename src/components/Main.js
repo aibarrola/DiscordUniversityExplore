7b3855a4b92939135e5f657e66c7453c00c8bb03
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-05-05 15:57:23 -0700

    Refactored the Card Component further

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,43 +3,41 @@ import Home from '../components/pages/Home';
 import Card from '../components/Card/card'
 import '../components/Card/card.css'
 
+
+
 function row() {
+
+    let row = []
+    function card() {
+        for(let i = 0; i < 3; i++) {
+            row.push(<Card orgName={"Card" + i} orgDesc="Desc" />)
+        }
+        return row;
+    }
+
     return(
         <div className ="cardContainer">
-                <Card orgName="Akbayan" orgDesc="Desc" />
-
-                <Card orgName="Akbayan" orgDesc="Desc" />
-
-                <Card orgName="Akbayan" orgDesc="Desc" />
+            {card()}
         </div>
     )
 }
 
+
 function displayRow() {
 
+    let rows = [];
+
     for(let i = 0; i < 3; i ++) {
-        return row();
+        rows.push(row());
     }
-
-
+    return rows;
 }
 
 function Main({page}){
-
-
-
     return(
         <div>
             <h4> {page} </h4> 
-
-            {/* list of organizations */}
-
             {displayRow()}
-            {displayRow()}
-            {displayRow()}
-
-            
-
         </div>
     )
 }
