d60d1197e716147f1b6b749ea6d555b04cd9dbc6
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2021-05-08 00:27:25 -0700

    Can now add new orgs

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,91 +3,21 @@ import Home from '../components/pages/Home';
 import Card from '../components/Card/card'
 import '../components/Card/card.css'
 
-let organizations = [ {orgName:"SASE", orgDesc:"This is SASE"},
-             {orgName:"SASE", orgDesc:"This is SASE"}, 
-             {orgName:"SASE", orgDesc:"This is SASE"}, 
-             {orgName:"SASE", orgDesc:"This is SASE"} ]
+let organizations = [{ orgName: "SASE", orgDesc: "This is SASE" },
+{ orgName: "SASE", orgDesc: "This is SASE" },
+{ orgName: "SASE", orgDesc: "This is SASE" },
+{ orgName: "SASE", orgDesc: "This is SASE" }]
+
+function Main({ page }) {
 
 
-// const state = {
-//     orgs: [ {orgName:"SASE", orgDesc:"This is SASE"},
-//             {orgName:"SASE", orgDesc:"This is SASE"}, 
-//             {orgName:"SASE", orgDesc:"This is SASE"}, 
-//             {orgName:"SASE", orgDesc:"This is SASE"} ]
-// }
+    const initialOrgData = Object.freeze({
+        orgName: "",
+        orgDesc: ""
+    });
 
-
-
-
-//returns a row 
-function toRow(row) {
-    let cardRow = []
-    function card() {
-        for(let i = 0; i < row.length; i++) {
-            cardRow.push(row[i]);
-        }
-        return cardRow;
-    }
-    return(
-        <div className ="cardContainer">
-            {card()}
-        </div>
-    )
-}
-
-function toCard(org) {
-    return(
-        <Card orgName={org.orgName} orgDesc={org.orgDesc} />
-    )
-}
-
-//pseudocode: if the row has hit 3, push the current row into rows, then start a new row
-function display() {
-    let row = [];
-    let rows = [];
-
-    //organize the cards
-    for(let  i = 0; i < organizations.length; i++) {
-        if((i+1) % 3 == 0 && i != 0) { 
-            row.push(toCard(organizations[i])); //push the 3rd col, before  adding the row
-            rows.push(row);
-            row = [];
-        } else {
-            row.push(toCard(organizations[i]));
-        }
-    }
-    if(row.length != 0) {
-        rows.push(row);
-    }
-
-    //display the cards
-
-    let newList  =[];
-    for(let i = 0; i < rows.length; i++) {
-        newList.push(toRow(rows[i])); //convert each row to a card component
-    }
-    return newList;
-
-}
-
-function AddCard(org) {
-    //const [orgs, setOrgs] = useState(organizations);
-    //const [orgs, dispatch] = React.useReducer(reducer, initialState);
-
-    organizations.push(org);
-    console.log("pushing new card: "); 
-    console.log(organizations[organizations.length-1]);;
-
-}
-
-
-const initialOrgData = Object.freeze({
-    orgName: "",
-    orgDesc: ""
-  });
-
-const OrgForm = () => {
     const [formData, updateFormData] = React.useState(initialOrgData);
+    const [orgs, setOrgs] = useState(organizations);
 
     const handleChange = (e) => {
         updateFormData({
@@ -95,39 +25,96 @@ const OrgForm = () => {
 
             // Trimming any whitespace
             [e.target.name]: e.target.value.trim()
-            
+
         });
     };
 
-    const handleSubmit = (e) => {
-        //e.preventDefault()
-        AddCard(formData);
-        
+    //returns a row 
+    function toRow(row) {
+        let cardRow = []
+        function card() {
+            for (let i = 0; i < row.length; i++) {
+                cardRow.push(row[i]);
+            }
+            return cardRow;
+        }
+        return (
+            <div className="cardContainer">
+                {card()}
+            </div>
+        )
+    }
+
+    function toCard(org) {
+        return (
+            <Card orgName={org.orgName} orgDesc={org.orgDesc} />
+        )
+    }
+
+    //pseudocode: if the row has hit 3, push the current row into rows, then start a new row
+    function display() {
+        let row = [];
+        let rows = [];
+
+        //organize the cards
+        for (let i = 0; i < orgs.length; i++) {
+            if ((i + 1) % 3 == 0 && i != 0) {
+                row.push(toCard(orgs[i])); //push the 3rd col, before  adding the row
+                rows.push(row);
+                row = [];
+            } else {
+                row.push(toCard(orgs[i]));
+            }
+        }
+        if (row.length != 0) {
+            rows.push(row);
+        }
+
+        //display the cards
+
+        let newList = [];
+        for (let i = 0; i < rows.length; i++) {
+            newList.push(toRow(rows[i])); //convert each row to a card component
+        }
+        return newList;
+
+    }
+
+    function addCard(org) {
+
+
+        setOrgs(organizations => [...organizations, org]);
+        console.log("pushing new card: ");
+        console.log(orgs[orgs.length - 1]);;
+
+    }
+
+
+
+
+    const orgForm = () => {
+
+        return (
+            <>
+                <label>
+                    Organization Name
+ <input name="orgName" onChange={handleChange} />
+                </label>
+                <br />
+                <label>
+                    Organization Description
+ <input name="orgDesc" onChange={handleChange} />
+                </label>
+                <br />
+                <button onClick={() => addCard(formData)}>Submit</button>
+            </>
+        );
     };
 
     return (
-        <>
-            <label>
-                Organization Name
-          <input name="orgName" onChange={handleChange} />
-            </label>
-            <br />
-            <label>
-                Organization Description
-          <input name="orgDesc" onChange={handleChange} />
-            </label>
-            <br />
-            <button onClick={handleSubmit}>Submit</button>
-        </>
-    );
-};
-
-
-function Main({page}){
-    return(
         <div>
-            <h4> {page} </h4> 
-            {OrgForm()}
+            <h4> {page} </h4>
+            {orgForm()}
             {display()}
         </div>
     )
